f38575015805c6f0a4ddc24866225020eef64435
Antoine Steyer <a.steyer@radioactivit.com>
2019-01-17 09:54:58 +0100

    Create RecipeHighlight component, a card containing the recipe of the day/moment

---
+++ src/components/RecipeHighlight.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <h1>Recette du moment</h1>
+      <v-card v-if="firstRecipe">
+        <v-img :src="firstRecipe.image" aspect-ratio="2"></v-img>
+
+        <v-card-title>
+          <div>
+            <h3>{{firstRecipe.name}}</h3>
+            <span>{{firstRecipe.country}}, {{firstRecipe.region}}</span>
+          </div>
+        </v-card-title>
+
+        <v-card-text v-if="firstRecipe.history">{{firstRecipe.history}}</v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>arrow_forward</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  computed: {
+    firstRecipe() {
+      const recipes = this.$store.getters["recipes/getRecipes"];
+      if (!recipes || recipes.length === 0) return null;
+
+      const randomIndex = this.getRandomInt(recipes.length);
+
+      return recipes[randomIndex];
+    }
+  },
+  methods: {
+    getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+  }
+};
+</script>
+<style scoped>
+.v-card__text {
+  text-align: left;
+}
+</style>
