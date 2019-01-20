b5cc67987672e3ae5e7377a03b418337c94ab800
Antoine Steyer <a.steyer@radioactivit.com>
2019-01-20 21:50:09 +0100

    Improve researchBar and indicate in country/region filter the nature of an item in autocomplete (country or region)

--- src/components/ResearchBar.vue
+++ src/components/ResearchBar.vue
@@ -18,8 +18,7 @@
           <v-icon
             v-on:click="iconClick('ingredientsSelected')"
             :style="ingredientsIconStyle"
-          >shopping_basket
-          </v-icon>
+          >shopping_basket</v-icon>
           <v-label>Par ingrédient</v-label>
         </v-container>
       </v-flex>
@@ -34,147 +33,169 @@
       :search-input.sync="searchForResults"
       :items="getItems()"
       hide-no-data
+      item-value="value"
+      item-text="value"
+      return-object
     >
       <template v-if="countrySelected" slot="item" slot-scope="{ item }">
-        <flag class="flagIcon" :iso="isoFromCountry(item)"/>
-        <span class="pr-2">{{ item}}</span>
+        <flag class="flagIcon" :squared="false" :iso="isoFromCountry(item.value)"/>
+        <span class="pr-2">{{ item.value}}</span>
+        <span class="isCountryOrRegion">{{item.isCountry ? ' (pays)' : ' (région)'}}</span>
       </template>
     </v-combobox>
   </v-container>
 </template>
 
 <script>
-  import {eventBus} from "../main";
+import { eventBus } from "../main";
 
-  export default {
-    name: "ResearchBar",
-    data () {
-      return {
-        countrySelected: false,
-        nameSelected: true,
-        ingredientsSelected: false,
-        placeholder: "nom",
-        searchFieldDisabled: false,
-        searchLabel: "Choisissez un critère",
-        researchValue: "",
-        searchForResults: null
-      };
+export default {
+  name: "ResearchBar",
+  data() {
+    return {
+      countrySelected: false,
+      nameSelected: true,
+      ingredientsSelected: false,
+      placeholder: "nom",
+      searchFieldDisabled: false,
+      searchLabel: "Choisissez un critère",
+      researchValue: "",
+      searchForResults: null
+    };
+  },
+  computed: {
+    countryIconStyle() {
+      return this.countrySelected
+        ? "border-style: solid; border-radius: 5px; padding: 10%; background-color: lightgrey;border-color: #157e0d;"
+        : "border-style: solid; border-radius: 5px; padding: 10%;";
     },
-    computed: {
-      countryIconStyle () {
-        return this.countrySelected
-          ? "border-style: solid; border-radius: 5px; padding: 10%; background-color: lightgrey;border-color: #157e0d;"
-          : "border-style: solid; border-radius: 5px; padding: 10%;";
-      },
-      nameIconStyle () {
-        return this.nameSelected
-          ? "border-style: solid; border-radius: 5px; padding: 10%; background-color: lightgrey;border-color: #157e0d;"
-          : "border-style: solid; border-radius: 5px; padding: 10%;";
-      },
-      ingredientsIconStyle () {
-        return this.ingredientsSelected
-          ? "border-style: solid; border-radius: 5px; padding: 10%; background-color: lightgrey;border-color: #157e0d;"
-          : "border-style: solid; border-radius: 5px; padding: 10%";
-      }
+    nameIconStyle() {
+      return this.nameSelected
+        ? "border-style: solid; border-radius: 5px; padding: 10%; background-color: lightgrey;border-color: #157e0d;"
+        : "border-style: solid; border-radius: 5px; padding: 10%;";
     },
-    methods: {
-      iconClick (variableName) {
-        let selected = this[variableName];
-        this[variableName] = !this[variableName];
-        this.searchFieldDisabled = selected;
-        this.researchValue = "";
-        this.searchLabel === "Choisissez un critère"
-          ? (this.searchLabel = "Entrez votre recherche")
-          : (this.searchLabel = "Choisissez un critère");
-        switch (variableName) {
-          case "countrySelected":
-            this.nameSelected = false;
-            this.ingredientsSelected = false;
-            if (selected) {
-              this.placeholder = "";
-            } else {
-              this.placeholder = "pays, région";
-            }
-            break;
-          case "nameSelected":
-            this.ingredientsSelected = false;
-            this.countrySelected = false;
-            if (selected) {
-              this.placeholder = "";
-            } else {
-              this.placeholder = "nom";
-            }
-            break;
-          case "ingredientsSelected":
-            this.nameSelected = false;
-            this.countrySelected = false;
-            if (selected) {
-              this.placeholder = "";
-            } else {
-              this.placeholder = "ingrédient1,...,ingrédientN";
-            }
-            break;
-          default:
+    ingredientsIconStyle() {
+      return this.ingredientsSelected
+        ? "border-style: solid; border-radius: 5px; padding: 10%; background-color: lightgrey;border-color: #157e0d;"
+        : "border-style: solid; border-radius: 5px; padding: 10%";
+    }
+  },
+  methods: {
+    iconClick(variableName) {
+      let selected = this[variableName];
+      this[variableName] = !this[variableName];
+      this.searchFieldDisabled = selected;
+      this.researchValue = "";
+      this.searchLabel === "Choisissez un critère"
+        ? (this.searchLabel = "Entrez votre recherche")
+        : (this.searchLabel = "Choisissez un critère");
+      switch (variableName) {
+        case "countrySelected":
+          this.nameSelected = false;
+          this.ingredientsSelected = false;
+          if (selected) {
             this.placeholder = "";
-        }
-      },
-
-      getCountriesAndRegions() {
-        const recipes = this.$store.getters["recipes/getRecipes"];
-        return recipes
-          .map(recipe => recipe.country)
-          .concat(recipes.map(recipe => recipe.region));
-      },
-      getItems() {
-        return this.nameSelected || this.ingredientsSelected
-          ? []
-          : this.getCountriesAndRegions();
-      },
-      isoFromCountry(country) {
-        switch (country) {
-          case "France":
-            return "fr";
-          case "Corse":
-            return "fr";
-          case "Ile de France":
-            return "fr";
-          case "Normandie":
-            return "fr";
-          case "Bretagne":
-            return "fr";
-          case "Etats-Unis":
-            return "us";
-          case "Japon":
-            return "jp";
-          default:
-            return;
-        }
+          } else {
+            this.placeholder = "pays, région";
+          }
+          break;
+        case "nameSelected":
+          this.ingredientsSelected = false;
+          this.countrySelected = false;
+          if (selected) {
+            this.placeholder = "";
+          } else {
+            this.placeholder = "nom";
+          }
+          break;
+        case "ingredientsSelected":
+          this.nameSelected = false;
+          this.countrySelected = false;
+          if (selected) {
+            this.placeholder = "";
+          } else {
+            this.placeholder = "ingrédient1,...,ingrédientN";
+          }
+          break;
+        default:
+          this.placeholder = "";
       }
     },
-    watch: {
-      searchForResults(val) {
-        this.researchValue = val;
-        let criterion = this.nameSelected
-          ? "name"
-          : this.countrySelected
-            ? "country"
-            : "ingredient";
-        console.log("searchForResults", this.researchValue, criterion);
-        this.$emit("search", this.researchValue, criterion);
+
+    getCountriesAndRegions() {
+      const recipes = this.$store.getters["recipes/getRecipes"];
+      return recipes
+        .map(recipe => {
+          return {
+            value: recipe.country,
+            isCountry: true
+          };
+        })
+        .concat(
+          recipes.map(recipe => {
+            return {
+              value: recipe.region,
+              isCountry: false
+            };
+          })
+        )
+        .filter(recipe => recipe.value);
+    },
+    getItems() {
+      return this.nameSelected || this.ingredientsSelected
+        ? []
+        : this.getCountriesAndRegions();
+    },
+    isoFromCountry(country) {
+      switch (country) {
+        case "France":
+          return "fr";
+        case "Corse":
+          return "fr";
+        case "Ile de France":
+          return "fr";
+        case "Normandie":
+          return "fr";
+        case "Bretagne":
+          return "fr";
+        case "Etats-Unis":
+          return "us";
+        case "Japon":
+          return "jp";
+        default:
+          return;
       }
     }
-  };
+  },
+  watch: {
+    searchForResults(val) {
+      if (typeof val === Object) {
+        this.researchValue = val.value;
+      } else {
+        this.researchValue = val;
+      }
+
+      let criterion = this.nameSelected
+        ? "name"
+        : this.countrySelected
+        ? "country"
+        : "ingredient";
+      console.log("searchForResults", this.researchValue, criterion);
+      this.$emit("search", this.researchValue, criterion);
+    }
+  }
+};
 </script>
 
 <style scoped>
-  .flag-icon {
-    margin-right: 10px;
-  }
-  .flagIcon {
-    border-style: solid;
-    border-color: lightslategrey;
-    border-width: 2px
-  }
+.flagIcon {
+  margin-right: 10px;
+  height: 16px;
+}
+
+.isCountryOrRegion {
+  color: rgba(0, 0, 0, 0.5);
+}
 </style>
 
 
