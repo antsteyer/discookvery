14a5f82fbd2dbbe5c5550cd0275b2f034b209b10
Antoine Steyer <a.steyer@radioactivit.com>
2019-01-20 11:26:54 +0100

    Handle filters in List

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,6 @@
 <template>
-  <v-card>
+  <v-card v-if="results.length > 0">
     <v-list three-line>
-      {{result()}}
       <template v-for="(item, index) in results">
         <v-list-tile avatar :key="index" v-on:click="visualizeDetails(item)">
           <v-list-tile-avatar>
@@ -18,59 +17,80 @@
           </v-list-tile-content>
         </v-list-tile>
 
-        <v-divider :key="'sep'+index" v-if="index + 1 < recipes.length"></v-divider>
+        <v-divider :key="'sep'+index" v-if="index + 1 < results.length"></v-divider>
       </template>
     </v-list>
   </v-card>
+  <span v-else>Pas de recettes correspondant aux critères de recherche</span>
 </template>
 
 <script>
-  import {eventBus} from "../main";
-
-  export default {
-
-    name: "List",
-    computed: {
-      recipes() {
-        return this.$store.getters["recipes/getRecipes"];
-      }
-    },
-    created() {
-      eventBus.$on('searchByCountryOrRegion', (message, criteria) => {
-        console.log('hello:', message, criteria);
-        //this.results = this.$store.getters["recipes/getRecipes"].filter(data => data.name.includes(search));
-      });
-      eventBus.$on('searchByName', (message) => {
-        this.frombrother = message;
-      });
-      eventBus.$on('searchByOther', (message) => {
-        this.frombrother = message;
-      });
-    },
-    data: function () {
-      return {
-        results: []
-      }
-    },
-    methods: {
-      result() {
-        this.results = this.$store.getters["recipes/getRecipes"];
-      },
-      visualizeDetails(recipe) {
-        this.$router.push({
-          name: 'RecipeDetails',
-          params: {
-            id: recipe.id,
-            recipe: recipe
-          }
-        });
-      }
+export default {
+  name: "List",
+  computed: {
+    recipes() {
+      return this.$store.getters["recipes/getRecipes"];
     }
-  };
+  },
+  created() {
+    // eventBus.$on("searchByCountryOrRegion", (message, criteria) => {
+    //   console.log("hello:", message, criteria);
+    //   //this.results = this.$store.getters["recipes/getRecipes"].filter(data => data.name.includes(search));
+    // });
+    // eventBus.$on("searchByName", message => {
+    //   this.frombrother = message;
+    // });
+    // eventBus.$on("searchByOther", message => {
+    //   this.frombrother = message;
+    // });
+  },
+  data: function() {
+    return {
+      results: this.recipes || []
+    };
+  },
+  mounted() {
+    this.results = this.recipes;
+  },
+  methods: {
+    result() {
+      this.results = this.$store.getters["recipes/getRecipes"];
+    },
+    filterList(value, criterion) {
+      console.log("Filter in List", value, criterion);
+      this.results = this.recipes.filter(r => {
+        if (criterion === "country") {
+          return (
+            r.country.toLowerCase().includes(value.toLowerCase()) ||
+            r.region.toLowerCase().includes(value.toLowerCase())
+          );
+        }
+        if (criterion === "name") {
+          return r.name.toLowerCase().includes(value.toLowerCase());
+        }
+        if (criterion === "ingredient") {
+          return r.ingredients
+            .map(i => i.toLowerCase())
+            .includes(value.toLowerCase());
+        }
+      });
+      console.log("list filtered", this.results);
+    },
+    visualizeDetails(recipe) {
+      this.$router.push({
+        name: "RecipeDetails",
+        params: {
+          id: recipe.id,
+          recipe: recipe
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
-  .v-icon {
-    margin-right: 5px;
-  }
+.v-icon {
+  margin-right: 5px;
+}
 </style>
