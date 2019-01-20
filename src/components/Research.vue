b6f43fa6cb8d0b51c2c022d9f9e2d25cf74396d6
Antoine Steyer <a.steyer@radioactivit.com>
2019-01-20 11:27:17 +0100

    Don't use eventBus in Research anymore, use refs instead

--- src/components/Research.vue
+++ src/components/Research.vue
@@ -1,27 +1,26 @@
 <template>
   <v-container>
     <ResearchBar v-on:search="refreshList"/>
-    <List @refresh/>
+    <List ref="recipeList"/>
   </v-container>
 </template>
 
 <script>
-  import ResearchBar from "./ResearchBar.vue";
-  import List from "./List";
-  export default {
-    name: 'Research',
-    components: {
-      ResearchBar,
-      List
-    },
-    methods: {
-      refreshList(value, criterion) {
-        this.$emit("refresh", value, criterion)
-      }
+import ResearchBar from "./ResearchBar.vue";
+import List from "./List";
+export default {
+  name: "Research",
+  components: {
+    ResearchBar,
+    List
+  },
+  methods: {
+    refreshList(value, criterion) {
+      this.$refs.recipeList.filterList(value, criterion);
     }
   }
+};
 </script>
 
 <style scoped>
-
 </style>
