104c56004630ee0825292b308f8716835b43ad10
Antoine Steyer <a.steyer@radioactivit.com>
2019-01-21 07:55:07 +0100

    Remove comma when recipe has no region in List

--- src/components/List.vue
+++ src/components/List.vue
@@ -9,7 +9,7 @@
 
           <v-list-tile-content>
             <v-list-tile-title>{{item.name}}</v-list-tile-title>
-            <v-list-tile-sub-title>{{item.country}}, {{item.region}}</v-list-tile-sub-title>
+            <v-list-tile-sub-title>{{item.country}}{{item.region ? `, ${item.region}`: ''}}</v-list-tile-sub-title>
             <v-layout row align-center>
               <v-icon small>star</v-icon>
               <span>{{item.rate}}</span>
